3c2e802484d0b153345a6ee39c7b6f49f89cb5e2
Santiago Ruiz <hiebra@hotmail.com>
2022-02-23 01:17:20 +0100

    Inicio de las hostilidades

---
+++ src/main/resources/archetype-resources/src/test/java/__class__Tests.java
@@ -0,0 +1,17 @@
+package ${package};
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ${class}Tests {
+	
+    @Test
+    void test() {
+        int two = 2;
+        if (1 + 1 != two) {
+            fail("Not yet implemented");   
+        }
+    }
+	
+}
